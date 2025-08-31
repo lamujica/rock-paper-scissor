05f43a088b22de21428f0abfe99404724d80646a
Leslie Mujica <35012536+lamujica@users.noreply.github.com>
2025-08-31 19:13:40 -0400

    create function getComputerChoice to obtain computers choice

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Hello World!");
+
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  let choice = choices[Math.floor(Math.random() * 3)];
+  console.log(choice);
+}
+
+getComputerChoice();
