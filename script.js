cf8e9dad174113600c7fe8f5402599be0f3b05a7
Leslie Mujica <35012536+lamujica@users.noreply.github.com>
2025-09-03 20:01:53 -0400

    create function playround to play a single round and return winner

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   let choice = choices[Math.floor(Math.random() * 3)];
-  console.log(choice);
+  return choice;
 }
 
 function getHumanChoice() {
   let choice = prompt("What would you like to play (rock, paper, or scissors)?");
-  console.log(choice);
+  return choice;
 }
 
 let humanScore = 0;
@@ -17,7 +17,16 @@ let computerScore = 0;
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
     console.log(`It is a tie! You selected: ${humanChoice} & Computer selected: ${computerChoice}`);
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log(`You selected: ${humanChoice} & Computer selected: ${computerChoice} --> Computer Wins!`);
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log(`You selected: ${humanChoice} & Computer selected: ${computerChoice} --> Computer Wins!`);
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log(`You selected: ${humanChoice} & Computer selected: ${computerChoice} --> Computer Wins!`);
+  } else {
+    console.log(`You selected: ${humanChoice} & Computer selected: ${computerChoice} --> The Human Race Wins! Congrats!`);
   }
 }
 
-playRound("scissors", "scissors");
+playRound(getHumanChoice(), getComputerChoice());
+console.log(`Human Score:${humanScore} Computer Score:${humanScore}`);
